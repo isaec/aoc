e5195fc62a3ec6bf9c5790e99d6a1ce0d1b46b8d
Isaac <isaac@rubenfamily.com>
2022-12-03 01:30:05 -0800

    use executor

--- days/three/solve.ts
+++ days/three/solve.ts
@@ -1,43 +1,66 @@
-import { readInput } from "../util.ts";
+// deno-lint-ignore-file require-await
+import Executor from "../executor.ts";
 
-const { lines, text } = await readInput(import.meta.url);
+const executor = new Executor(import.meta.url);
 
 const priorityStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const getPriority = (c: string) => priorityStr.indexOf(c) + 1;
 
-let sum = 0;
+await executor.part1(async ({ text, lines }) => {
+  let sum = 0;
 
-// for (const line of lines) {
-//   // split line in half by length
-//   // line has no spaces
-//   const half = line.length / 2;
-//   const firstHalf = line.slice(0, half);
-//   const secondHalf = line.slice(half);
+  for (const line of lines) {
+    // split line in half by length
+    // line has no spaces
+    const half = line.length / 2;
+    const firstHalf = line.slice(0, half);
+    const secondHalf = line.slice(half);
 
-//   // find the item in both halves
-//   const item = firstHalf.split("").find((c) => secondHalf.includes(c));
-//   if (item === undefined) throw Error("no item found");
+    // find the item in both halves
+    const item = firstHalf.split("").find((c) => secondHalf.includes(c));
+    if (item === undefined) throw Error("no item found");
 
-//   // add to sum
-//   sum += getPriority(item);
-// }
+    // add to sum
+    sum += getPriority(item);
+  }
 
-// pt2
+  return sum;
+});
 
-// take the lines in chunks of 3
-for (let i = 0; i < lines.length; i += 3) {
-  const first = lines[i];
-  const second = lines[i + 1];
-  const third = lines[i + 2];
+await executor.part2(async ({ text, lines }) => {
+  let sum = 0;
 
-  // find the item in all three
-  const item = first
-    .split("")
-    .find((c) => second.includes(c) && third.includes(c));
-  if (item === undefined) throw Error("no item found");
+  // for (const line of lines) {
+  //   // split line in half by length
+  //   // line has no spaces
+  //   const half = line.length / 2;
+  //   const firstHalf = line.slice(0, half);
+  //   const secondHalf = line.slice(half);
 
-  // add to sum
-  sum += getPriority(item);
-}
+  //   // find the item in both halves
+  //   const item = firstHalf.split("").find((c) => secondHalf.includes(c));
+  //   if (item === undefined) throw Error("no item found");
 
-console.log(sum);
+  //   // add to sum
+  //   sum += getPriority(item);
+  // }
+
+  // pt2
+
+  // take the lines in chunks of 3
+  for (let i = 0; i < lines.length; i += 3) {
+    const first = lines[i];
+    const second = lines[i + 1];
+    const third = lines[i + 2];
+
+    // find the item in all three
+    const item = first
+      .split("")
+      .find((c) => second.includes(c) && third.includes(c));
+    if (item === undefined) throw Error("no item found");
+
+    // add to sum
+    sum += getPriority(item);
+  }
+  return sum;
+});
