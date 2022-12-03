baff8824204801d6400b61bef1f4c05478bff920
Isaac <isaac@rubenfamily.com>
2022-12-03 12:58:07 -0800

    shorten executor name

--- days/template/solve.ts
+++ days/template/solve.ts
@@ -1,18 +1,18 @@
 // deno-lint-ignore-file require-await
 import Executor from "../executor.ts";
 
-const executor = new Executor(import.meta.url);
+const ex = new Executor(import.meta.url);
 
-await executor.part1(async ({ text, lines }) => {});
+await ex.part1(async ({ text, lines }) => {});
 
-await executor.testPart1([
+await ex.testPart1([
   ["description", [`input`, "expected"]],
   ["description", [`input`, "expected"]],
 ]);
 
-await executor.part2(async ({ text, lines }) => {});
+await ex.part2(async ({ text, lines }) => {});
 
-await executor.testPart2([
+await ex.testPart2([
   ["description", [`input`, "expected"]],
   ["description", [`input`, "expected"]],
 ]);
