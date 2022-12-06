8a536001a88996b26e8f72bd021c182b52314d6f
Isaac <isaac@rubenfamily.com>
2022-12-05 20:58:45 -0800

    d6

---
+++ src/days/six/solve.ts
@@ -0,0 +1,34 @@
+// deno-lint-ignore-file require-await
+import {} from "@util/set.ts";
+import {} from "@util/collect.ts";
+import Executor from "@util/executor.ts";
+
+/* the example input
+
+*/
+
+const ex = new Executor(import.meta.url);
+
+await ex.part1(async ({ text, lines }, console) => {});
+
+await ex.testPart1([
+  ex.c`
+input
+`(),
+
+  ex.c`
+input
+`(),
+]);
+
+await ex.part2(async ({ text, lines }, console) => {});
+
+await ex.testPart2([
+  ex.c`
+input
+`(),
+
+  ex.c`
+input
+`(),
+]);
