4817a89c2c178b7390de655792bcf7959721c065
Isaac <isaac@rubenfamily.com>
2022-12-05 21:08:21 -0800

    solve d6

--- src/days/six/solve.ts
+++ src/days/six/solve.ts
@@ -3,30 +3,80 @@ import {} from "@util/set.ts";
 import {} from "@util/collect.ts";
 import Executor from "@util/executor.ts";
 
-/* the example input
+/* Here are a few more examples:
+
+bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
 
 */
 
 const ex = new Executor(import.meta.url);
 
-await ex.part1(async ({ text, lines }, console) => {});
+const allDifferent = (ch: string[]) => {
+  const set = new Set(ch);
+  return set.size === ch.length;
+};
+
+await ex.part1(async ({ text, lines }, console) => {
+  // detect first four characters that are all different
+
+  let processed = 0;
+
+  const chars = text.split("");
+  const charSection: string[] = [];
+  for (let i = 0; i < 4; i++) {
+    charSection.unshift(chars.shift()!);
+    processed++;
+  }
+
+  while (!allDifferent(charSection)) {
+    // console.log("section", charSection.join(""));
+    charSection.unshift(chars.shift()!);
+    charSection.pop();
+    processed++;
+  }
+
+  return processed;
+});
 
 await ex.testPart1([
   ex.c`
-input
-`(),
+bvwbjplbgvbhsrlpgdmjqwftvncz
+`(5),
 
   ex.c`
-input
-`(),
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+`(11),
 ]);
 
-await ex.part2(async ({ text, lines }, console) => {});
+await ex.part2(async ({ text, lines }, console) => {
+  // detect first four characters that are all different
+
+  let processed = 0;
+
+  const chars = text.split("");
+  const charSection: string[] = [];
+  for (let i = 0; i < 14; i++) {
+    charSection.unshift(chars.shift()!);
+    processed++;
+  }
+
+  while (!allDifferent(charSection)) {
+    // console.log("section", charSection.join(""));
+    charSection.unshift(chars.shift()!);
+    charSection.pop();
+    processed++;
+  }
+
+  return processed;
+});
 
 await ex.testPart2([
   ex.c`
-input
-`(),
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+`(19),
 
   ex.c`
 input
