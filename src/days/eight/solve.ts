304e418f3ded585ea88aee681bd12557211fc519
Isaac <isaac@rubenfamily.com>
2022-12-07 20:59:21 -0800

    d8

---
+++ src/days/eight/solve.ts
@@ -0,0 +1,38 @@
+// deno-lint-ignore-file require-await
+import Executor from "@util/executor.ts";
+
+/* the example input
+
+*/
+
+const ex = new Executor(import.meta.url);
+
+// deno-lint-ignore no-unused-vars
+await ex.part1(async ({ text, lines }, console, tick) => {
+  // goal:
+});
+
+await ex.testPart1([
+  ex.c`
+input
+`(),
+
+  ex.c`
+input
+`(),
+]);
+
+// deno-lint-ignore no-unused-vars
+await ex.part2(async ({ text, lines }, console, tick) => {
+  // goal:
+});
+
+await ex.testPart2([
+  ex.c`
+input
+`(),
+
+  ex.c`
+input
+`(),
+]);
