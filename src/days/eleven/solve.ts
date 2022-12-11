75afa357d5389a11afe69be0eb6d6b84b6bd90fe
Isaac <isaac@rubenfamily.com>
2022-12-10 21:49:47 -0800

    finish pt1 start pt2

--- src/days/eleven/solve.ts
+++ src/days/eleven/solve.ts
@@ -35,45 +35,48 @@ const ex = new Executor(import.meta.url);
 
 type Monkey = {
   id: number;
-  items: number[];
-  operation: (old: number) => number;
-  test: (worryLevel: number) => boolean;
+  items: bigint[];
+  operation: (old: bigint) => bigint;
+  test: (worryLevel: bigint) => boolean;
   ifTrueTarget: number;
   ifFalseTarget: number;
 };
 
-const operationParser = (operation: string): ((old: number) => number) => {
+const operationParser = (operation: string): ((old: bigint) => bigint) => {
   const [_left, right] = operation.split(" = ");
   const [_, mathOp, value] = right.split(" ");
 
-  const numValue =
-    value === "old" ? (old: number) => old : (_old: number) => Number(value);
+  if (value === "old") {
+    return (old) => old * old;
+  }
+
+  const numValue = BigInt(value);
   switch (mathOp) {
     case "*":
-      return (old: number) => old * numValue(old);
+      return (old) => old * numValue;
     case "+":
-      return (old: number) => old + numValue(old);
+      return (old) => old + numValue;
     case "-":
-      return (old: number) => old - numValue(old);
+      return (old) => old - numValue;
     case "/":
-      return (old: number) => old / numValue(old);
+      return (old) => old / numValue;
     default:
       throw new Error(`Unknown math operation: ${mathOp}`);
   }
 };
 
-const testParser = (test: string): ((worryLevel: number) => boolean) => {
+const testParser = (test: string): ((worryLevel: bigint) => boolean) => {
   // always divisible by, so we can just check if the number is divisible by the number
   const words = test.split(" ");
-  const numValue = Number(words[2]);
+  const numValue = BigInt(words[2]);
   // console.log({ test, numValue });
-  return (worryLevel: number) => worryLevel % numValue === 0;
+  return (worryLevel: bigint) => worryLevel % numValue === 0n;
 };
 
 const monkeyParser = (input: string): Monkey => {
   const lines = input.split("\n");
   const id = Number(lines[0].split(" ")[1].slice(0, -1));
-  const items = lines[1].split(": ")[1].split(", ").map(Number) ?? [];
+  const items = lines[1].split(": ")[1].split(", ").map(BigInt) ?? [];
   const operation = lines[2].split(": ")[1];
   const test = lines[3].split(": ")[1];
   const ifTrueTarget = Number(lines[4].split(": ")[1].split(" ")[3]);
@@ -118,26 +121,29 @@ await ex.part1(async ({ text, lines }, console, tick) => {
   for (let r = 1; r <= totalRounds; r++) {
     for (const monkey of monkeys) {
       // monkey inspect all of its items, one at a time
+      console.log(`Monkey ${monkey.id}:`);
       while (monkey.items.length > 0) {
         tick();
-        console.log(`Monkey ${monkey.id} inspecting item ${monkey.items[0]}`);
         monkeyInspections[monkey.id] += 1;
 
         const itemWorryLevel = monkey.items.shift();
         if (itemWorryLevel === undefined) throw new Error("No item to inspect");
 
-        const newItem = monkey.operation(itemWorryLevel);
+        const postOpItem = monkey.operation(itemWorryLevel);
+        const newItem = postOpItem / 3n;
 
-        console.log(`Item ${itemWorryLevel} becomes ${newItem}`);
+        console.log(
+          `\tItem ${itemWorryLevel} becomes ${postOpItem} and then ${newItem}`
+        );
 
         if (monkey.test(newItem)) {
           console.log(
-            `Item ${newItem} is true, throwing to monkey ${monkey.ifTrueTarget}`
+            `\t\tItem ${newItem} is true, throwing to monkey ${monkey.ifTrueTarget}`
           );
           monkeys[monkey.ifTrueTarget].items.push(newItem);
         } else {
           console.log(
-            `Item ${newItem} is false, throwing to monkey ${monkey.ifFalseTarget}`
+            `\t\tItem ${newItem} is false, throwing to monkey ${monkey.ifFalseTarget}`
           );
           monkeys[monkey.ifFalseTarget].items.push(newItem);
         }
@@ -147,6 +153,10 @@ await ex.part1(async ({ text, lines }, console, tick) => {
     console.log(`Round ${r} complete`);
   }
   console.log(monkeyInspections);
+
+  // multiply the two highest numbers
+  const sorted = monkeyInspections.sort((a, b) => b - a);
+  return sorted[0] * sorted[1];
 });
 
 await ex.testPart1([
@@ -178,7 +188,7 @@ Monkey 3:
   Test: divisible by 17
     If true: throw to monkey 0
     If false: throw to monkey 1
-`(),
+`(10605),
 
   ex.c`
 input
@@ -187,7 +197,51 @@ input
 
 // deno-lint-ignore no-unused-vars
 await ex.part2(async ({ text, lines }, console, tick) => {
-  // goal:
+  const monkeyStrings = text.split("\n\n");
+  const monkeys = monkeyStrings.map(monkeyParser);
+
+  const totalRounds = 10000;
+
+  const monkeyInspections = monkeys.map(() => 0);
+
+  for (let r = 1; r <= totalRounds; r++) {
+    for (const monkey of monkeys) {
+      // monkey inspect all of its items, one at a time
+      console.log(`Monkey ${monkey.id}:`);
+      for (
+        let i = 0;
+        i < monkey.items.length;
+        i++, monkeyInspections[monkey.id]++
+      ) {
+        const itemWorryLevel = monkey.items[i];
+        if (itemWorryLevel === undefined) throw new Error("No item to inspect");
+
+        const newItem = monkey.operation(itemWorryLevel);
+
+        console.log(`\tItem ${itemWorryLevel} becomes ${newItem}`);
+
+        if (monkey.test(newItem)) {
+          console.log(
+            `\t\tItem ${newItem} is true, throwing to monkey ${monkey.ifTrueTarget}`
+          );
+          monkeys[monkey.ifTrueTarget].items.push(newItem);
+        } else {
+          console.log(
+            `\t\tItem ${newItem} is false, throwing to monkey ${monkey.ifFalseTarget}`
+          );
+          monkeys[monkey.ifFalseTarget].items.push(newItem);
+        }
+      }
+      monkey.items = [];
+      console.log(`Monkey ${monkey.id} has no more items to inspect\n`);
+    }
+    window.console.log(`Round ${r} complete`);
+  }
+  console.log(monkeyInspections);
+
+  // multiply the two highest numbers
+  const sorted = monkeyInspections.sort((a, b) => b - a);
+  return sorted[0] * sorted[1];
 });
 
 await ex.testPart2([
