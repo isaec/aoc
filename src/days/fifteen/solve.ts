388f78cff6c985b929d8c235ccbb9e716ae93e06
Isaac <isaac@rubenfamily.com>
2022-12-14 22:28:58 -0800

    pt2

--- src/days/fifteen/solve.ts
+++ src/days/fifteen/solve.ts
@@ -81,7 +81,109 @@ await ex.part1(async ({ text, lines }, console, tick) => {
   return occupied.size;
 });
 
-await ex.testPart1([
+// await ex.testPart1([
+//   ex.c`
+// Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+// Sensor at x=9, y=16: closest beacon is at x=10, y=16
+// Sensor at x=13, y=2: closest beacon is at x=15, y=3
+// Sensor at x=12, y=14: closest beacon is at x=10, y=16
+// Sensor at x=10, y=20: closest beacon is at x=10, y=16
+// Sensor at x=14, y=17: closest beacon is at x=10, y=16
+// Sensor at x=8, y=7: closest beacon is at x=2, y=10
+// Sensor at x=2, y=0: closest beacon is at x=2, y=10
+// Sensor at x=0, y=11: closest beacon is at x=2, y=10
+// Sensor at x=20, y=14: closest beacon is at x=25, y=17
+// Sensor at x=17, y=20: closest beacon is at x=21, y=22
+// Sensor at x=16, y=7: closest beacon is at x=15, y=3
+// Sensor at x=14, y=3: closest beacon is at x=15, y=3
+// Sensor at x=20, y=1: closest beacon is at x=15, y=3
+// `(26)(true),
+
+//   ex.c`
+// input
+// `()(false),
+// ]);
+
+// deno-lint-ignore no-unused-vars
+await ex.part2(async ({ text, lines }, console, tick) => {
+  // if (lines.length > 20) return;
+
+  type Elem = "B" | "S" | "." | "#";
+  const grid = new InfiniteGrid2d<Elem>(".");
+
+  class Diamond {
+    public center: Point2d;
+
+    constructor(
+      public readonly x: number,
+      public readonly y: number,
+      public readonly radius: number
+    ) {
+      this.center = new Point2d(x, y);
+    }
+
+    contains(point: Point2d) {
+      return point.manhattanDistanceTo(this.center) <= this.radius;
+    }
+  }
+
+  const diamonds: Diamond[] = [];
+
+  for (const line of lines) {
+    const [x1, y1] = line
+      .split(":")[0]
+      .slice(10)
+      .split(", ")
+      .map((e) => Number(e.slice(2)));
+
+    const sensorPoint = new Point2d(x1, y1);
+    grid.setPoint(sensorPoint, "S");
+
+    const [x2, y2] = line
+      .split(":")[1]
+      .slice(" closest beacon is at ".length)
+      .split(", ")
+      .map((e) => Number(e.slice(2)));
+
+    const beaconPoint = new Point2d(x2, y2);
+    grid.setPoint(beaconPoint, "B");
+
+    const distance = sensorPoint.manhattanDistanceTo(beaconPoint);
+
+    const diamond = new Diamond(x1, y1, distance);
+    diamonds.push(diamond);
+  }
+
+  const minX = 0;
+  const maxX = 4000000;
+  const minY = 0;
+  const maxY = 4000000;
+
+  for (let x = minX; x <= maxX; x++) {
+    window.console.log(x);
+    for (let y = minY; y <= maxY; y++) {
+      let contained = false;
+      for (const diamond of diamonds) {
+        if (diamond.contains(new Point2d(x, y))) {
+          contained = true;
+          // increase y to skip the rest of the diamond
+          const distanceToBorder = diamond.center.manhattanDistanceTo(
+            new Point2d(x, y)
+          );
+          y += diamond.radius - distanceToBorder;
+          break;
+        }
+      }
+      if (!contained) {
+        return x * 4_000_000 + y;
+      }
+    }
+  }
+
+  grid.print();
+});
+
+await ex.testPart2([
   ex.c`
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
@@ -97,22 +199,7 @@ Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3
-`(26)(true),
-
-  ex.c`
-input
-`()(false),
-]);
-
-// deno-lint-ignore no-unused-vars
-await ex.part2(async ({ text, lines }, console, tick) => {
-  // goal:
-});
-
-await ex.testPart2([
-  ex.c`
-input
-`()(false),
+`(56000011)(true),
 
   ex.c`
 input
