99ef8cef59dc9e82116ed489d3fc5f3fe83b417e
Isaac <isaac@rubenfamily.com>
2022-12-15 22:41:39 -0800

    copy pt1 for pt2

--- src/days/sixteen/solve.ts
+++ src/days/sixteen/solve.ts
@@ -119,19 +119,116 @@ await ex.part1(async ({ text, lines }, console, tick) => {
   });
 });
 
-await ex.testPart1([
-  exampleInput(1651)(true),
+// await ex.testPart1([
+//   exampleInput(1651)(true),
 
-  ex.c`
-input
-`()(false),
-]);
+//   ex.c`
+// input
+// `()(false),
+// ]);
 
 // deno-lint-ignore no-unused-vars
-await ex.part2(async ({ text, lines }, console, tick) => {});
+await ex.part2(async ({ text, lines }, console, tick) => {
+  const graph = new Graph<string>();
+
+  const valves = new Map<string, number>();
+
+  const startingClosedValves = new Set<string>();
+
+  for (const line of lines) {
+    const [, name, flowRate] = line.match(/Valve (\w+) has flow rate=(\d+);/)!;
+    // console.log({ name, flowRate });
+    valves.set(name, Number(flowRate));
+    if (flowRate !== "0") startingClosedValves.add(name);
+
+    // console.log(line);
+
+    const tunnels = line
+      .match(/tunnels? leads? to valves? (\w+(?:, \w+)*)/)![1]
+      .split(", ");
+
+    // console.log(tunnels);
+
+    graph.addEdges(name, tunnels);
+  }
+
+  type EvalChoice = {
+    valve: string;
+    cost: number;
+    value: number;
+  };
+
+  type State = {
+    currentValve: string;
+    remainingTime: number;
+    totalFlow: number;
+    closedValves: Set<string>;
+  };
+
+  const pathCache = new Map<string, number>();
+  const getPathLength = (from: string, to: string): number => {
+    const key = `${from} -> ${to}`;
+    if (pathCache.has(key)) return pathCache.get(key)!;
+    const pathLength = graph.shortestPath(from, to)!.length;
+    pathCache.set(key, pathLength);
+    return pathLength;
+  };
+
+  const getChoices = (state: State) => {
+    const choices = Array.from(state.closedValves.keys());
+
+    const evalChoices: EvalChoice[] = [];
+
+    for (const choice of choices) {
+      const cost = getPathLength(state.currentValve, choice);
+      const value = valves.get(choice)! * (state.remainingTime - cost);
+      evalChoices.push({ valve: choice, cost, value });
+    }
+
+    return evalChoices.sort((a, b) => b.value - a.value);
+  };
+
+  const applyChoice = (choice: EvalChoice, state: State) => {
+    state.totalFlow += choice.value;
+    state.remainingTime -= choice.cost;
+    state.currentValve = choice.valve;
+    state.closedValves.delete(choice.valve);
+  };
+
+  const copyState = (state: State): State => ({
+    currentValve: state.currentValve,
+    remainingTime: state.remainingTime,
+    totalFlow: state.totalFlow,
+    closedValves: new Set(state.closedValves),
+  });
+
+  const getBestBranchFlow = (state: State): number => {
+    const choices = getChoices(state);
+    const flows = choices.map((choice) => {
+      const nextState = copyState(state);
+      if (choice.cost > nextState.remainingTime) {
+        return state.totalFlow;
+      }
+      applyChoice(choice, nextState);
+      if (nextState.closedValves.size === 0) {
+        return nextState.totalFlow;
+      }
+      return getBestBranchFlow(nextState);
+    });
+
+    return Math.max(...flows);
+  };
+
+  return getBestBranchFlow({
+    currentValve: "AA",
+    remainingTime: 26,
+    totalFlow: 0,
+    closedValves: startingClosedValves,
+  });
+});
 
 await ex.testPart2([
-  exampleInput()(true),
+  exampleInput(1707)(true),
 
   ex.c`
 input
