6a2b7b7459c48c5b695f1cda08d1cfbdbfc983e7
Isaac <isaac@rubenfamily.com>
2023-01-29 00:21:17 -0800

    finish up the demo

--- src/days/robo/solve.ts
+++ src/days/robo/solve.ts
@@ -7,15 +7,29 @@ const ex = new Executor(import.meta.url);
 
 enum FieldValues {
   Empty = " ",
-  Element = "▩",
+  Element = "⛶",
   Path = "◌",
   Point = "◉",
 }
+
+type Rectangle = {
+  from: Point2d;
+  to: Point2d;
+};
+
+const rectangle = (from: Point2d, to: Point2d): Rectangle => ({
+  from,
+  to,
+});
 
 // deno-lint-ignore no-unused-vars
 await ex.part1(async ({ text, lines }, console, tick) => {
   // skip actual input
   if (lines[0].charAt(0) !== "(") return;
+
+  lines.forEach((line) => {
+    console.log(line);
+  });
 
   const field = new Grid2d<FieldValues>(FieldValues.Empty, 0, 20, 0, 20);
   // set the outer border
@@ -29,24 +43,30 @@ await ex.part1(async ({ text, lines }, console, tick) => {
       field.setPoint(point, FieldValues.Element);
     }
   });
-  // put the start and end points
-  const sourcePoints = text.split(" to ");
-  const from = new Point2d(
-    Number(sourcePoints[0].split(", ")[0].slice(1)),
-    Number(sourcePoints[0].split(", ")[1].slice(0, -1))
-  );
-  const to = new Point2d(
-    Number(sourcePoints[1].split(", ")[0].slice(1)),
-    Number(sourcePoints[1].split(", ")[1].slice(0, -1))
-  );
+  // get the start and end points
+  const sourcePoints = lines[0].split(" to ");
+  const from = Point2d.fromString(sourcePoints[0]);
+  const to = Point2d.fromString(sourcePoints[1]);
 
+  // get all rectangle points
+  const rectanglePoints = lines.slice(1).map((line) => {
+    const [from, to] = line.split(" from ")[1].split(" to ");
+    const rect = rectangle(Point2d.fromString(from), Point2d.fromString(to));
+    for (let x = rect.from.x; x <= rect.to.x; x++) {
+      for (let y = rect.from.y; y <= rect.to.y; y++) {
+        field.set(x, y, FieldValues.Element);
+      }
+    }
+  });
+
+  // show the start and end points
   field.setPoint(from, FieldValues.Point);
   field.setPoint(to, FieldValues.Point);
 
   // build a graph
-  const graph = new Graph<Point2d>();
+  const graph = new Graph<ReturnType<Point2d["toString"]>>();
   field.iterate().forEach(([value, point]) => {
-    graph.addNode(point);
+    graph.addNode(point.toString());
     if (value === FieldValues.Element) return;
     const neighbors = [
       new Point2d(point.x - 1, point.y),
@@ -61,18 +81,50 @@ await ex.part1(async ({ text, lines }, console, tick) => {
 
     neighbors.forEach((neighbor) => {
       if (field.getPoint(neighbor) === FieldValues.Element) return;
-      graph.addEdge(point, neighbor);
+      graph.addEdge(point.toString(), neighbor.toString());
     });
   });
 
   // find the shortest path
-  const path = graph.shortestPath(from, to);
+  const path = graph.shortestPath(from.toString(), to.toString());
 
   if (!path) return;
 
   // draw the path
   path.forEach((point) => {
-    field.setPoint(point, FieldValues.Path);
+    if (point === from.toString() || point === to.toString()) return;
+    field.setPoint(Point2d.fromString(point), FieldValues.Path);
+  });
+
+  // find the spline control points
+  // every coordinate that ends a straight or diagonal line segment is a control point
+  const controlPoints = path.filter((point, index) => {
+    if (index === 0 || index === path.length - 1) return false;
+    const prev = Point2d.fromString(path[index - 1]);
+    const next = Point2d.fromString(path[index + 1]);
+    const current = Point2d.fromString(point);
+    // if the previous and next point are on the same line horizontal or vertical
+    if (prev.x === current.x && current.x === next.x) return false;
+    if (prev.y === current.y && current.y === next.y) return false;
+
+    // if the previous and next point are on the same diagonal
+    if (
+      prev.x - current.x === prev.y - current.y &&
+      current.x - next.x === current.y - next.y
+    )
+      return false;
+    if (
+      prev.x - current.x === current.y - prev.y &&
+      current.x - next.x === next.y - current.y
+    )
+      return false;
+
+    return true;
+  });
+
+  // draw the control points
+  controlPoints.forEach((point) => {
+    field.setPoint(Point2d.fromString(point), FieldValues.Point);
   });
 
   field.print();
@@ -80,6 +132,11 @@ await ex.part1(async ({ text, lines }, console, tick) => {
 
 await ex.testPart1([
   ex.c`
-(3, 3) to (10, 10)
+(3, 3) to (11, 18)
+element rect from (6, 9) to (9, 12)
+element rect from (6, 9) to (12, 9)
+element rect from (6, 9) to (6, 17)
+element rect from (5, 5) to (6, 6)
+element rect from (10, 14) to (11, 15)
 `()(true),
 ]);
